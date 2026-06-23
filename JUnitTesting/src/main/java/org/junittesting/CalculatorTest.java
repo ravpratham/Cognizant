8a9d726d542a036087ed904f636e5c77989d6bcf
Pratham Rav <ravpratham223@gmail.com>
2026-06-23 18:25:21 +0530

    J Unit Testing

---
+++ JUnitTesting/src/main/java/org/junittesting/CalculatorTest.java
@@ -0,0 +1,24 @@
+package org.junittesting;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class CalculatorTest {
+    Calculator cal = new Calculator();
+
+    @Test
+    public void testAdd() {
+        assertEquals(5, cal.add(2,3));  // 5 == 2 + 3 - True
+    }
+
+    @Test
+    public void testSubtract() {
+        assertNotEquals(5, cal.subtract(2,3));  // 5 != 2 + 3 - True
+    }
+
+    @Test
+    public void testMultiply() {
+        assertTrue(5 > cal.multiply(2,3));  // 5 > 2 * 3 - False
+    }
+
+}
